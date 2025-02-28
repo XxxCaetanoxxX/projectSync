ba323d5a83252a25d1ce2fec1d49bbaebe3cef74
Caetano César <caetanocesar35@gmail.com>
2025-02-27 21:24:36 -0300

    fix: finalizando testes

--- src/party_house/party_house.service.spec.ts
+++ src/party_house/party_house.service.spec.ts
@@ -4,6 +4,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 describe('PartyHouseService', () => {
   let service: PartyHouseService;
+  let createdPartyHouseId: number;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,4 +17,31 @@ describe('PartyHouseService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should find all party houses', async () => {
+    const result = await service.findAll({});
+    expect(result.length).toBeGreaterThan(0);
+  });
+
+  it('should find one party house', async () => {
+    const result = await service.findOne(1);
+    expect(result).toMatchObject({ id: 1, name: 'star 415', address: 'Rua 1' });;
+  });
+
+  it('should create a party house', async () => {
+    const result = await service.create({ name: 'chalezinho', address: 'Rua 3' });
+    createdPartyHouseId = result.id;
+    expect(Object.keys(result)).toEqual(['id', 'name', 'address']);
+  });
+
+  it('should update a party house', async () => {
+    const partyHouse = await service.findOne(createdPartyHouseId);
+    const result = await service.update(createdPartyHouseId, { name: 'chalezinho2' });
+    expect(result.name).not.toEqual(partyHouse.name);
+  });
+
+  it('should delete party house', async () => {
+    const result = await service.remove(createdPartyHouseId);
+    expect(result).toBeDefined();
+  })
 });
--- src/pdf/pdf.service.spec.ts
+++ src/pdf/pdf.service.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PdfService } from './pdf.service';
+import { EventService } from '../event/event.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('PdfService', () => {
   let service: PdfService;
+  let eventService: EventService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PdfService],
+      providers: [PdfService, EventService, PrismaService],
     }).compile();
 
+    eventService = module.get<EventService>(EventService);
     service = module.get<PdfService>(PdfService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should generate pdf', async () => {
+    const event = await eventService.findOne(1);
+    const result = await service.generatePdf(event);
+    expect(result).toBeInstanceOf(Buffer);
   });
 });
