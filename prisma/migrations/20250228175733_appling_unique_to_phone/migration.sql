072bea79f95e443ea02b0da8a03d6f78d2f3fc2b
Caetano César <caetanocesar35@gmail.com>
2025-03-18 21:49:17 -0300

    fea: mudando banco de dados

--- prisma/migrations/20250228175733_appling_unique_to_phone/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[phone]` on the table `tb_user` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `tb_user_phone_key` ON `tb_user`(`phone`);
