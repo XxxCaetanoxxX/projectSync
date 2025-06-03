e8876ec6e306d2dac952955aa1434da5e2911c6f
caetano.cesar <caetano.cesar@defensoria.mg.def.br>
2025-06-03 08:15:08 -0300

    fix: tickets body swagger

--- src/ticket/dto/create-ticket-type.dto.ts
+++ src/ticket/dto/create-ticket-type.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString, MaxLength } from "class-validator";
 
 export class CreateTicketTypeDto {
+    @ApiProperty()
     @MaxLength(30)
     @IsString()
     name: string;
 
+    @ApiProperty()
     @IsNumber()
     price: number;
 
+    @ApiProperty()
     @IsNumber()
     quantity: number;
 
+    @ApiProperty()
     @IsNumber()
     eventId: number
 }
--- src/ticket/dto/find-all-ticket.dto.ts
+++ src/ticket/dto/find-all-ticket.dto.ts
@@ -1,10 +1,13 @@
+import { ApiPropertyOptional } from "@nestjs/swagger"
 import { IsNumber, IsOptional } from "class-validator"
 
 export class FindAllTicketDto {
+    @ApiPropertyOptional()
     @IsOptional()
     @IsNumber()
     userId?: number
 
+    @ApiPropertyOptional()
     @IsOptional()
     @IsNumber()
     ticketTypeId?: number
