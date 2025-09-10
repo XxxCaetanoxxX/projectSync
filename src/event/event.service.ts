3f36eb6aca2af0f38e2a6516f937c785004e0eae
Caetano César <caetanocesar35@gmail.com>
2025-09-10 19:45:47 -0300

    fix: corrigindo metodo findAll

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -66,13 +66,6 @@ export class EventService {
             id: true,
             name: true
           }
-        },
-        ticketTypes: {
-          select: {
-            name: true,
-            price: true,
-            quantity: true
-          }
         }
       },
       skip,
