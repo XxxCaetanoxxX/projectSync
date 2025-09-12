a05d16d406297090ca19f335eef732f230a74d61
Caetano César <caetanocesar35@gmail.com>
2025-09-11 22:19:36 -0300

    fix: corrigindo migrations

--- prisma/migrations/20250616151556_removendo_quantity_da_tabela_hist_de_batch/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `quantity` on the `th_batch_hist` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "th_batch_hist" DROP COLUMN "quantity";
--- prisma/migrations/20250709194659_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[code]` on the table `tb_ticket` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "tb_ticket_code_key" ON "tb_ticket"("code");
