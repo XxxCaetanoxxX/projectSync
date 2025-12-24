d62e04e895053bcb60599800d3dd6afde0079787
Caetano César <caetanocesar35@gmail.com>
2025-12-24 19:49:09 -0300

    fea: preparando para deploy aws; fix: cirrigindo endpoints

---
+++ prisma/migrations/20251202203642_adicionando_auth_type_na_tabela_de_hist_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "th_user_hist" ADD COLUMN     "authType" "AuthType" NOT NULL DEFAULT 'CREDENTIAL';
---
+++ prisma/migrations/20251202204019_adicionando_refresh_token_a_tb_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "th_user_hist" ADD COLUMN     "refresh_token" TEXT;
